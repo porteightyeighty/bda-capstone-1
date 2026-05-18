3aff53cf367ce7127696bf40a2da7b8015ab47c5
Matt Port <mattp0@duck.com>
2026-05-18 18:51:07 +0100

    phase 5 - sequentially downloading videos

---
+++ reports/sequential_report.md
@@ -0,0 +1,11 @@
+# Report
+
+## Serial execution
+
+Total time:
+22.05
+
+## Complexity
+
+Time complexity: O(n)
+Space complexity: O(1)
--- solutions/main.py
+++ solutions/main.py
@@ -1,7 +1,21 @@
 from library import download_video, read_video_urls
+import time
 
 
 if __name__ == "__main__":
     download_video("https://www.youtube.com/watch?v=jNQXAC9IVRw")
-    read_video_urls("data/video_urls.csv")
-
+    urls = read_video_urls("data/video_urls.csv")
+    total_start = time.perf_counter()
+    for url in urls:
+        start = time.perf_counter()
+        download_video(url)
+        end = time.perf_counter()
+        elapsed = end - start
+        serial_time = round(elapsed, 2)
+        print(f"Download Time: {serial_time}")
+    total_end = time.perf_counter()
+    total_elapsed = total_end - total_start
+    total_serial_time = round(total_elapsed, 2)
+    # with open('reports/sequential_report.md', 'a', encoding='utf-8') as f:
+    #     f.write(str(total_serial_time))
+    print(f"Sequential Execution: {total_serial_time}")
